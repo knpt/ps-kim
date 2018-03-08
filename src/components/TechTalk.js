e6cbcc487b5f6fa4c8a5db7d01ab9127081aa2dd
Kimberly Ton <kimberlyton@gmail.com>
2018-03-08 00:28:08 -0500

    fix: disable outoplay and refactor

--- src/components/TechTalk.js
+++ src/components/TechTalk.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import YouTube from 'react-youtube'
 
-class TechTalk extends React.Component {
-  render() {
-    const opts = {
-      height: '390',
-      width: '640',
-      playerVars: {
-        autoplay: 1
-      }
-    };
-
-    return (
-      <YouTube
-        videoId="d7pyEDqBDeE"
-        opts={opts}
-        onReady= {this._onReady}
-      />
-    )
+const opts = {
+  height: '390',
+  width: '640',
+  playerVars: {
+    autoplay: 0
   }
 }
+
+const TechTalk = () => (
+  <YouTube
+    videoId="d7pyEDqBDeE"
+    opts={opts}
+  />
+)
+
 
 export default TechTalk
